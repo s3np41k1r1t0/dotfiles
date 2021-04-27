d882461468504808c906fd13c1cbf15cce568ae7
s3np41k1r1t0 <brunomendes@tecnico.ulisboa.pt>
2021-04-27 11:35:47 +0100

    removed unfinished polybar

--- polybar/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-killall -q polybar
-
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-polybar monitor1 -r &
-
-#xrandr --output eDP1 --mode 1920x1080 --rate 75 --output HDMI1 --off &
