486923b49978fff66701700bb502a68cddac9014
s3np41k1r1t0 <brunomendes@tecnico.ulisboa.pt>
2022-06-28 03:15:49 +0100

    fixed script

--- make_links.sh
+++ make_links.sh
@@ -9,6 +9,7 @@ case $1 in
 		;;
 esac
 
+mkdir -p $HOME/.config/nvim
 ln -s `pwd`/nvim/init.vim $HOME/.config/nvim/init.vim 
 ln -s `pwd`/nvim/coc-settings.json $HOME/.config/nvim/coc-settings.json 
 ln -s `pwd`/tmux/.tmux.conf $HOME/.tmux.conf
