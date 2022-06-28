92c6ae0654723f3720f825d61e105cdbb5fd365a
s3np41k1r1t0 <brunomendes@tecnico.ulisboa.pt>
2022-06-28 03:22:50 +0100

    fixed make links script

--- make_links.sh
+++ make_links.sh
@@ -4,6 +4,6 @@
 HOME=/home/s3np41k1r1t0
 
 mkdir -p $HOME/.config/nvim
-ln -s `pwd`/nvim/init.vim $HOME/.config/nvim/init.vim 
-ln -s `pwd`/nvim/coc-settings.json $HOME/.config/nvim/coc-settings.json 
-ln -s `pwd`/tmux/.tmux.conf $HOME/.tmux.conf
+ln -s $HOME/dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim 
+ln -s $HOME/dotfiles/nvim/coc-settings.json $HOME/.config/nvim/coc-settings.json 
+ln -s $HOME/dotfiles/tmux/.tmux.conf $HOME/.tmux.conf
